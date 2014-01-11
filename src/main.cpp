2a1f5be58aa6d753a3a57d6e0b1efffad517dcf7
hansswaan <hansswans@outlook.com>
2014-01-11 08:56:15 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 // Copyright (c) 2011-2013 The Moneyenom developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-// 1432 fee destruction correction
+
 
 #include "checkpoints.h"
 #include "db.h"
